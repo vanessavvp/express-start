15c2848b74aa5bff2199fb4e3bd5b3afc201f11e
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2015-03-03 15:57:01 +0000

    routes

---
+++ routes/gulpfile.js
@@ -0,0 +1,22 @@
+var gulp    = require('gulp');
+var shell = require('gulp-shell');
+
+//    gutil   = require('gulp-util');
+
+gulp.task('default', ['server']);
+
+gulp.task('server', function () {
+  return gulp.src('').pipe(shell([ 'node app.js' ]));
+});
+
+gulp.task('put', function() {
+  return gulp.src('').pipe(shell(["curl -X PUT -v localhost:3000/user"]));
+});
+
+gulp.task('delete', function() {
+  return gulp.src('').pipe(shell("curl -X DELETE -v localhost:3000/user"));
+});
+
+gulp.task('post', function() {
+  return gulp.src('').pipe(shell("curl -X POST -v -d 'ignored data' localhost:3000"));
+});
