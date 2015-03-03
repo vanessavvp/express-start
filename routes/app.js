c255ec1f1cb474addde6d441957f860c29b86404
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2015-03-03 20:41:52 +0000

    static files

--- routes/app.js
+++ routes/app.js
@@ -2,13 +2,17 @@ var express = require('express')
 var app = express()
 var path = require('path');
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 // respond with "Hello World!" on the homepage
 app.get('/', function (req, res) {
-  res.send('Got a GET request');
+  res.send('Got a GET request'+
+    '<br/><img src="images/kitten.jpg" />'
+  );
 })
 
 // accept POST request on the homepage
