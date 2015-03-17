5f4df13f0a89c9b4a1cb6c25c75844c25d41ac75
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2015-03-17 21:08:29 +0000

    form example (name)

--- hello/hello.js
+++ hello/hello.js
@@ -6,25 +6,45 @@ var path = require('path');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-  //res.send('Hello World!')
-  res.render('index', { title: 'Express' });
-})
+// Serve static files
+app.use(express.static('.'));
+
+app.set('port', (process.env.PORT || 8000));
+
+var express = require('express');
 
 /*
- var router = express.Router();
-  module.exports = router; 
-*/
-app.get('/chuchu', function (req, res) {
-  //res.send('Hello Chuchu!')
-  res.render('index', { title: 'Chuchu' });
-})
+ * body-parser is a piece of express middleware that 
+ *   reads a form's input and stores it as a javascript
+ *   object accessible through `req.body` 
+ *
+ * 'body-parser' must be installed (via `npm install --save body-parser`)
+ * For more info see: https://github.com/expressjs/body-parser
+ */
 
-var server = app.listen(3000, function () {
+// instruct the app to use the `bodyParser()` middleware for all routes
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }));
 
-  var host = server.address().address
-  var port = server.address().port
+// A browser's default method is 'GET', so this
+// is the route that express uses when we visit
+// our site initially.
+app.get('/', function(req, res){
+  // The form's action is '/' and its method is 'POST',
+  // so the `app.post('/', ...` route will receive the
+  // result of our form
+  res.render('index', { title: "form"});
+});
 
-  console.log('Example app listening at http://%s:%s', host, port)
+// This route receives the posted form.
+// As explained above, usage of 'body-parser' means
+// that `req.body` will be filled in with the form elements
+app.post('/', function(req, res){
+  var userName = req.body.userName;
+  res.render('greet', {userName: userName});
+});
 
-})
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
+
