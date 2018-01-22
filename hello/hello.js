aad3a95e064b3d9adb1479a0715c73433e8a1ae3
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2018-01-22 12:42:22 +0000

    computing ip address via ip module

--- hello/hello.js
+++ hello/hello.js
@@ -1,3 +1,4 @@
+var ip = require("ip");
 var express = require('express')
 var app = express()
 var path = require('path');
@@ -32,7 +33,7 @@ app.get('/dog', function (req, res) {
   res.sendFile(path.join(__dirname, 'public/dog.jpg'));
 });
 
-var server = app.listen(8080, function () {
+var server = app.listen(8080, ip.address(), function () {
 
   var host = server.address().address
   var port = server.address().port
